ab5a94c8e8ee81ff5a0e6c0234e10451bd4597ec
datphung28-debug <datphung28@gmail.com>
2025-06-26 11:52:17 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [`resources/views/**/*`],
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: ["resources/views/**/*"],
         }),
         tailwindcss(),
     ],
+    build: {
+        manifest: true,
+        outDir: "public/build",
+    },
     server: {
+        host: "0.0.0.0", // Đảm bảo Railway/Render có thể truy cập
+        port: 5173,
         cors: true,
     },
 });
